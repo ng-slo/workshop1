55b775d40ec6feee68dedb1e3626778097fc3b15
Angular Slovenia <angularslo@gmail.com>
2017-03-06 11:25:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var message = "Hi";
 
 for (var i=0; i < 5; i++) {
-  console.log("Hi num. " + i)
+  console.log("Hi human " + i)
 }
+
+console.log("Total hi's " + i)
+
+for (var j=i; j>0; j--) {
+ console.log("Bye human: " + j) 
+}
+
+console.log("Total bye's " + j)
