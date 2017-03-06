ddd8764bafffe0b1bcefb83b18cf72ff492777f9
Nejc Zdovc <NejcZdovc@users.noreply.github.com>
2017-03-06 12:30:35 +0100

    Create npm.js

---
+++ npm.js
@@ -0,0 +1,18 @@
+var users = [
+  {
+    firstName: 'Janez',
+    lastName: 'Novak'
+  },
+  {
+    firstName: 'Franc',
+    lastName: 'Horvat'
+  },
+  {
+    firstName: 'Lojzka',
+    lastName: 'Kranjc'
+  },
+  {
+    firstName:'Micka',
+    lastName: 'Kovač'
+  }
+];
