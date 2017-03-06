1513787ef975ece04711af672094f7abe67ab07b
Nejc Zdovc <NejcZdovc@users.noreply.github.com>
2017-03-06 12:26:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 for (var i=0; i < 5; i++) {
-  console.log("Hi human " + i)
+  console.log("Hi human " + i);
 }
 
-console.log("Total hi's " + i)
+console.log("Total hi's " + i);
 
 for (var j=i; j>0; j--) {
- console.log("Bye human: " + j) 
+ console.log("Bye human: " + j);
 }
 
-console.log("Total bye's " + j)
+console.log("Total bye's " + j);
+
+var users = [
+  {
+    firstName: 'Janez',
+    lastName: 'Novak'
+  },
+  {
+    firstName: 'Franc',
+    lastName: 'Horvat'
+  },
+  {
+    firstName: 'Lojzka',
+    lastName: 'Kranjc'
+  },
+  {
+    firstName:'Micka',
+    lastName: 'Kovač'
+  }
+];
